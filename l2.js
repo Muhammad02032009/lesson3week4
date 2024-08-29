8a26e4e8e108e7a8cb579879313d2b70319b953f
Muhammad <muhammadarbobzoda@gmail.com>
2024-08-29 15:30:11 +0300

    jhgykh,.b

---
+++ l2.js
@@ -0,0 +1,47 @@
+// //task1
+// function time(n) {
+//     return n.getMonth()+1==10 && n.getDate()==31 ? "Bonfire toffee" : "toffee"
+// }
+// console.log(time(new Date("2013/10/31")));
+
+
+
+// //task2
+// function num(n1)
+// {
+//     return Math.ceil(n1/100)
+// }
+// console.log(num(2005));
+
+
+
+// //task3
+// function m(n)
+// {
+//     let j=["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+//     return j[n-1]
+// }
+// console.log(m(1));
+
+
+
+// //task4
+// function num(a,b)
+// {
+//     // return a+(Math.floor(b/12))
+//     return new Date(a,b).getFullYear()
+// }
+// console.log(num(2020, 24));
+
+
+
+// //task5
+// function n(n1)
+// {
+//     return n1.split("/").toReversed().join("")
+// }
+// console.log(n("11/12/2019"));
+
+
+
+
